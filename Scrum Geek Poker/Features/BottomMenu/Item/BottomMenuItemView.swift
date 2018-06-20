824c45c8796124290fccfe744e72aca644ad9b84
Tiago Oliveira <tiago_fernandes89@hotmail.com>
2018-06-20 00:20:20 -0300

    Linking layout to view

---
+++ Scrum Geek Poker/Features/BottomMenu/Item/BottomMenuItemView.swift
@@ -0,0 +1,21 @@
+//
+//  BottomMenuItemView.swift
+//  Scrum Geek Poker
+//
+//  Created by Tiago Oliveira on 20/06/18.
+//  Copyright © 2018 Optimize 7. All rights reserved.
+//
+
+import UIKit
+
+class BottomMenuItemView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
